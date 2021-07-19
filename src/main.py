d63d463ace4de45154a321b95bfcf1d4bf6443ed
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2021-07-19 23:29:10 +0300

    Changed base framework to async one

--- src/main.py
+++ src/main.py
@@ -1,48 +1,48 @@
-#!/usr/bin/env python
-
 import logging
 import os
 
+import emoji
+from aiogram import types, Bot, Dispatcher
+from aiogram.utils import executor
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from src import searcher
 
 load_dotenv()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 token = os.getenv('TOKEN')
-updater = Updater(token=token)
-dispatcher = updater.dispatcher
+
+# Initialize bot and dispatcher
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
 
-def start(update, context):
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
     """
-    /start command handler
-    :param update:
-    :param context:
-    :return:
+    This handler will be called when user sends `/start` or `/help` command
     """
-    context.bot.send_message(chat_id=update.effective_chat.id, text="What do you want to search?")
+    user = message.from_user.username
+    if user is None:
+        user = message.from_user.full_name
+    await message.reply(f"Hi {user}!\nI'm SearchBot!\nWhat do you want to search?")
 
 
-# TODO add search usage here
-def read_input(update, context):
-    """
-    Reads regular user's input
-    :param update: This module contains an object that represents a Telegram Update
-    :param context:
-    :return: modified message
-    """
-    answer = f'Agreed that {update.message.text}'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+@dp.message_handler(regexp='^\w+|^\d+')
+async def echo(message: types.Message):
+    search_results = await searcher.find(message.text)
+    ans = 'Results:\n'
+    # For now send back only first five findings
+    for result in search_results['books'][:5]:
+        ans += f"""
+        {emoji.emojize(':books:')} : {result['book_name']}
+        {emoji.emojize(':man:')} : {result['author']}
+        {emoji.emojize(':link:')} : {result['book_link']}
+        ------------------------------------------------------------
+        """
+    await message.answer(ans)
 
 
-# Actually handlers
-regular_message_handler = MessageHandler(Filters.text & (~Filters.command), read_input)
-start_handler = CommandHandler('start', start)
-
-# All registered handlers will be here
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(regular_message_handler)
-
-# The BEGINNING
-updater.start_polling()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
