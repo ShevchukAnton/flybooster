f0f20f17b121f10fbca205141fb3464607be848c
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2021-08-06 19:22:08 +0300

    Added check for available book's formats

--- enums/download_formats.py
+++ enums/download_formats.py
@@ -1,9 +1,18 @@
 from enum import Enum
 
+links = {
+    'FB2': 'fb2.zip',
+    'MOBI': 'fb2.mobi',
+    'EPUB': 'fb2.epub',
+    'PDF': '.pdf',
+    'DJVU': '.djvu',
+}
+
 
 class Formats(Enum):
     FB2 = 'fb2'
     EPUB = 'epub'
     MOBI = 'mobi'
     # djvu or PDF represented as direct download
-    DOWNLOAD = 'download'
+    PDF = 'download'
+    DJVU = 'download'
