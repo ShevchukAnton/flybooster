30f1bbf825e17ff126accf32f034bbe363605c09
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2022-06-23 01:27:35 +0300

    added continuous books upload

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import logging
 import os
 
 import emoji
-from aiogram import types, Bot, Dispatcher
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message
+from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
 from dotenv import load_dotenv
 
-from src import searcher
+from src import searcher, message_helper
 
 load_dotenv()
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 token = os.getenv('TOKEN')
 
@@ -17,9 +19,11 @@ token = os.getenv('TOKEN')
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+all_books = []
+
 
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
+async def start_handler(message: Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
@@ -30,35 +34,48 @@ async def start_handler(message: types.Message):
 
 
 @dp.message_handler(regexp='^\w+|^\d+')
-async def echo(message: types.Message):
+async def echo(message: Message):
     logging.info(f'User {message.from_user.username} - {message.from_user.full_name} requested for {message.text}')
+
     search_results = await searcher.find(message.text)
     if len(search_results['books']) == 0:
         ans = f"{emoji.emojize(':disappointed_face:')} Мы не смогли ничего найти по запросу: \"***{message.text}***\""
     else:
-        ans = await create_a_message(search_results)
+        ans = await create_a_message(search_results['books'])
 
-    await message.answer(ans.replace('_', ' '), parse_mode='Markdown')
+    if len(all_books) > 0:
+        inline_button = InlineKeyboardButton('Показать все', callback_data='show_all')
+        inline_markup = InlineKeyboardMarkup().add(inline_button)
+
+        await message.answer(ans.replace('_', ' '), parse_mode='Markdown', reply_markup=inline_markup)
+    else:
+        await message.answer(ans.replace('_', ' '), parse_mode='Markdown')
 
 
-async def create_a_message(content):
-    ans = f'Книг найдено: ***{len(content["books"])}***\n'
-    if len(content["books"]) > 5:
-        ans = f'Книг найдено: ***{len(content["books"])}*** (первые 5 будут показаны):\n'
-    
+@dp.callback_query_handler(func=lambda cb: cb.data == 'show_all')
+async def show_all_books(callback_query: CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    msg = create_a_message(all_books, None)
+
+    await bot.send_message(callback_query.from_user.id, msg)
+
+
+async def create_a_message(content, limit=5):
+    books_amount = len(content)
+    ans = f'Книг найдено: ***{books_amount}***\n'
+
     # For now send back only first five findings
-    books = await searcher.find_downloadable_formats(content['books'][:5])
-    for result in books:
-        links = ''
-        for fmt, link in result.get('fmts').items():
-            links += f"{emoji.emojize(':link:')} : [{fmt}]({link})\n            "
+    books = await searcher.find_downloadable_formats(content[:limit])
 
-        ans += f"""
-            {emoji.emojize(':books:')} : {result.get('book_name', 'Безымянная книга')}
-            {emoji.emojize(':memo:')} : {result.get('author', 'Автор не установлен')}
-            {links}
-            -------------------------------------------------------
-            """
+    if limit is not None and books_amount > limit:
+        all_books = content['books']
+        ans = f'Книг найдено: ***{books_amount}*** (первые {limit} будут показаны):\n'
+
+    ans += await message_helper.compile_message(books)
+
+    all_books.remove(books)
+
     return ans
 
 
---
+++ src/message_helper.py
@@ -0,0 +1,19 @@
+import emoji
+
+
+async def compile_message(books_list: list):
+    msg = ''
+
+    # For now send back only first five findings
+    for book in books_list:
+        links = ''
+        for fmt, link in book.get('fmts').items():
+            links += f"{emoji.emojize(':link:')} : [{fmt}]({link})\n            "
+
+        msg += f"""
+            {emoji.emojize(':books:')} : {book.get('book_name', 'Безымянная книга')}
+            {emoji.emojize(':memo:')} : {book.get('author', 'Автор не установлен')}
+            {links}
+            -------------------------------------------------------
+            """
+    return msg
