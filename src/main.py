a6874b8be218c100770b9113485b62967b7248cf
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2023-09-12 16:08:23 +0100

    Added dockerisation, removed moskalska mova

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ class Author_request(StatesGroup):
 
 
 all_books = []
-starting_search_msg = f"Приступаем к скурпулёзному поиску {emoji.emojize(':flashlight:')}"
+starting_search_msg = f"Починаємо ретельний пошук {emoji.emojize(':flashlight:')}"
 
 
 @dp.message_handler(commands=['start'])
@@ -45,33 +45,33 @@ async def start_handler(message: Message):
     user = message.from_user.username
     if user is None:
         user = message.from_user.full_name
-    await message.reply(f"Ку-ку {user}!\nЯ поисковый бот для книг!\nЧто изволите искать?")
+    await message.reply(f"Вітаю {user}!\nЯ пошуковий бот для книг!\n/name - пошук аз назвою \n/author - пошук за автором \n/genre - пошук за жанром \n/series - пошук за серією")
 
 
 @dp.message_handler(commands=['name'])
 async def echo(message: Message):
-    search_question = 'Введите название книги, или его часть'
+    search_question = 'Введіть назву книги або частину назви'
     await Name_request.query.set()
     await message.answer(search_question)
 
 
 @dp.message_handler(commands=['author'])
 async def search_by_author(message: Message):
-    search_question = 'Введите ФИО автора, или часть данных'
+    search_question = 'Введіть П.І.Б автора або якусь частину з них'
     await Author_request.query.set()
     await message.answer(search_question)
 
 
-@dp.message_handler(commands=['name'])
+@dp.message_handler(commands=['genre'])
 async def echo(message: Message):
-    search_question = 'Введите название книги, или его часть'
+    search_question = 'Введіть бажаний жанр'
     await Name_request.query.set()
     await message.answer(search_question)
 
 
-@dp.message_handler(commands=['name'])
+@dp.message_handler(commands=['series'])
 async def echo(message: Message):
-    search_question = 'Введите название книги, или его часть'
+    search_question = 'Введіть назву серії або частину назви'
     await Name_request.query.set()
     await message.answer(search_question)
 
@@ -87,13 +87,12 @@ async def query_handler(message: Message, state: FSMContext):
 
     search_results = await searcher.search_by_name(message.text)
     if len(search_results['books']) == 0:
-        ans = f"{emoji.emojize(':disappointed_face:')} Мы не смогли ничего найти по запросу: \"***{message.text}***\""
+        ans = f"{emoji.emojize(':man_shrugging:')} Вибачайте, ми не знайшли нічого за запитом: \"***{message.text}***\""
     else:
         ans = await create_a_message(search_results['books'])
 
     if len(all_books) > 0:
-        txt = 'Показать ещё 5' if len(
-            all_books) > 5 else f'Показать {len(all_books)}'
+        txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
 
         inline_button = InlineKeyboardButton(txt, callback_data='show_more')
         inline_markup = InlineKeyboardMarkup().add(inline_button)
@@ -107,20 +106,18 @@ async def query_handler(message: Message, state: FSMContext):
 async def query_handler(message: Message, state: FSMContext):
     await state.finish()
 
-    logging.info(
-        f'User {message.from_user.username} - {message.from_user.full_name} requested for {message.text}')
+    logging.info(f'User {message.from_user.username} - {message.from_user.full_name} requested for {message.text}')
 
     await message.answer(starting_search_msg, parse_mode='Markdown')
 
     search_results = await searcher.search_by_author(message.text)
     if len(search_results['authors']) == 0:
-        ans = f"{emoji.emojize(':disappointed_face:')} Мы не смогли ничего найти по запросу: \"***{message.text}***\""
+        ans = f"{emoji.emojize(':man_shrugging:')} Вибачайте, ми не знайшли нічого за запитом: \"***{message.text}***\""
     else:
         ans = await create_a_message(search_results['authors'])
 
     if len(all_books) > 0:
-        txt = 'Показать ещё 5' if len(
-            all_books) > 5 else f'Показать {len(all_books)}'
+        txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
 
         inline_button = InlineKeyboardButton(txt, callback_data='show_more')
         inline_markup = InlineKeyboardMarkup().add(inline_button)
@@ -134,15 +131,14 @@ async def query_handler(message: Message, state: FSMContext):
 async def show_more_books(callback_query: CallbackQuery):
     global all_books
     await bot.answer_callback_query(callback_query.id)
-    loading_more_books_msg = f'Перерываем архив поиска {emoji.emojize(":zzz:")}'
+    loading_more_books_msg = f'Шукаємо в архіві {emoji.emojize(":zzz:")}'
 
     await bot.send_message(callback_query.from_user.id, loading_more_books_msg, parse_mode='Markdown')
 
     msg = await create_a_message(all_books)
 
     if len(all_books) > 0:
-        txt = 'Показать ещё 5' if len(
-            all_books) > 5 else f'Показать {len(all_books)}'
+        txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
 
         inline_button = InlineKeyboardButton(txt, callback_data='show_more')
         inline_markup = InlineKeyboardMarkup().add(inline_button)
@@ -155,13 +151,13 @@ async def show_more_books(callback_query: CallbackQuery):
 async def create_a_message(content, limit=5):
     global all_books
     books_amount = len(content)
-    ans = f'Книг найдено: ***{books_amount}***\n'
+    ans = f'Книг знайдено: ***{books_amount}***\n'
 
     books = await searcher.find_downloadable_formats(content[:limit])
 
     if limit is not None and books_amount > limit:
         all_books = content
-        ans = f'Книг найдено: ***{books_amount}*** (первые {limit} будут показаны):\n'
+        ans = f'Книг знайдено: ***{books_amount}*** (перші {limit} відображені):\n'
 
     ans += await message_helper.compile_message(books)
 
