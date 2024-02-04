c67930da48fd51fa0bcf8aaf3a7e18773fadf209
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2024-02-04 02:41:49 +0000

    removed links, added buttons now bot sends back files instead of links

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import random
+import string
 import sys
+
 sys.path.append('.')
 
 from src import searcher, message_helper
@@ -12,7 +15,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQu
 import emoji
 import logging
 import os
-
+import urllib.request as request
 
 load_dotenv()
 logging.basicConfig(
@@ -29,6 +32,7 @@ dp = Dispatcher(bot, storage=storage)
 class Name_request(StatesGroup):
     query = State()
 
+
 class Author_request(StatesGroup):
     query = State()
 
@@ -37,20 +41,18 @@ all_books = []
 starting_search_msg = f"Починаємо ретельний пошук {emoji.emojize(':flashlight:')}"
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['help'])
 async def start_handler(message: Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    global all_books
-    all_books.clear()
     user = message.from_user.username
     if user is None:
         user = message.from_user.full_name
-    await message.reply(f"Вітаю {user}!\nЯ пошуковий бот для книг!\n/name - пошук аз назвою \n/author - пошук за автором \n/genre - пошук за жанром \n/series - пошук за серією")
+    await message.reply(f"Вітаю {user}!\nЯ пошуковий бот для книг!\n/name - пошук за назвою")
 
 
-@dp.message_handler(commands=['name'])
+@dp.message_handler(commands=['name', 'start'])
 async def echo(message: Message):
     global all_books
     all_books.clear()
@@ -59,7 +61,7 @@ async def echo(message: Message):
     await message.answer(search_question)
 
 
-@dp.message_handler(commands=['author'])
+# @dp.message_handler(commands=['author'])
 async def search_by_author(message: Message):
     global all_books
     all_books.clear()
@@ -68,7 +70,7 @@ async def search_by_author(message: Message):
     await message.answer(search_question)
 
 
-@dp.message_handler(commands=['genre'])
+# @dp.message_handler(commands=['genre'])
 async def echo(message: Message):
     global all_books
     all_books.clear()
@@ -77,7 +79,7 @@ async def echo(message: Message):
     await message.answer(search_question)
 
 
-@dp.message_handler(commands=['series'])
+# @dp.message_handler(commands=['series'])
 async def echo(message: Message):
     global all_books
     all_books.clear()
@@ -98,21 +100,26 @@ async def query_handler(message: Message, state: FSMContext):
     search_results = await searcher.search_by_name(message.text)
     if len(search_results['books']) == 0:
         ans = f"{emoji.emojize(':man_shrugging:')} Вибачайте, ми не знайшли нічого за запитом: \"***{message.text}***\""
+        await message.answer(ans, parse_mode='Markdown')
     else:
-        ans = await create_a_message(search_results['books'])
+        # ans = await create_a_message(search_results['books'])
+        messages = await aggregate_messages(search_results['books'])
 
-    if len(all_books) > 0:
-        txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
+        for txt, btns in messages.items():
+            markup = InlineKeyboardMarkup()
+            for btn in btns:
+                markup.add(btn)
 
-        inline_button = InlineKeyboardButton(txt, callback_data='show_more')
-        inline_markup = InlineKeyboardMarkup().add(inline_button)
+            if len(all_books) > 0 and txt == list(messages.keys())[-1]:
+                inline_txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
 
-        await message.answer(ans.replace('_', ' '), parse_mode='Markdown', reply_markup=inline_markup)
-    else:
-        await message.answer(ans.replace('_', ' '), parse_mode='Markdown')
+                inline_button = InlineKeyboardButton(inline_txt, callback_data='show_more')
+                markup.add(inline_button)
+
+            await message.answer(txt, parse_mode='Markdown', reply_markup=markup)
 
 
-@dp.message_handler(state=Author_request.query)
+# @dp.message_handler(state=Author_request.query)
 async def query_handler(message: Message, state: FSMContext):
     await state.finish()
 
@@ -122,9 +129,9 @@ async def query_handler(message: Message, state: FSMContext):
 
     search_results = await searcher.search_by_author(message.text)
     if len(search_results['authors']) == 0:
-        ans = f"{emoji.emojize(':man_shrugging:')} Вибачайте, ми не знайшли нічого за запитом: \"***{message.text}***\""
+        ans = f"{emoji.emojize(':man_shrugging:')} Вибачайте, ми не знайшли нічого за запитом: \"***{message.text.strip()}***\""
     else:
-        ans = await create_a_message(search_results['authors'])
+        ans = await aggregate_messages(search_results['authors'])
 
     if len(all_books) > 0:
         txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
@@ -132,9 +139,9 @@ async def query_handler(message: Message, state: FSMContext):
         inline_button = InlineKeyboardButton(txt, callback_data='show_more')
         inline_markup = InlineKeyboardMarkup().add(inline_button)
 
-        await message.answer(ans.replace('_', ' '), parse_mode='Markdown', reply_markup=inline_markup)
+        await message.answer(ans, parse_mode='Markdown', reply_markup=inline_markup)
     else:
-        await message.answer(ans.replace('_', ' '), parse_mode='Markdown')
+        await message.answer(ans, parse_mode='Markdown')
 
 
 @dp.callback_query_handler(lambda cb: cb.data == 'show_more')
@@ -145,35 +152,47 @@ async def show_more_books(callback_query: CallbackQuery):
 
     await bot.send_message(callback_query.from_user.id, loading_more_books_msg, parse_mode='Markdown')
 
-    msg = await create_a_message(all_books)
+    msg = await aggregate_messages(all_books)
 
-    if len(all_books) > 0:
-        txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
+    for txt, btns in msg.items():
+        markup = InlineKeyboardMarkup()
+        for btn in btns:
+            markup.add(btn)
 
-        inline_button = InlineKeyboardButton(txt, callback_data='show_more')
-        inline_markup = InlineKeyboardMarkup().add(inline_button)
+        if len(all_books) > 0 and txt == list(msg.keys())[-1]:
+            inline_txt = 'Показати ще 5' if len(all_books) > 5 else f'Показати {len(all_books)}'
 
-        await bot.send_message(callback_query.from_user.id, msg.replace('_', ' '), parse_mode='Markdown', reply_markup=inline_markup)
-    else:
-        await bot.send_message(callback_query.from_user.id, msg.replace('_', ' '), parse_mode='Markdown')
+            inline_button = InlineKeyboardButton(inline_txt, callback_data='show_more')
+            markup.add(inline_button)
+
+        await bot.send_message(callback_query.from_user.id, txt, parse_mode='Markdown', reply_markup=markup)
 
 
-async def create_a_message(content, limit=5):
+@dp.callback_query_handler(lambda cb: 'download**' in cb.data)
+async def download_book_and_send_to_user(cb: CallbackQuery):
+    url = cb.data.split("**")[1]
+    extension = url.split('/')[-1]
+    file_name = ''.join(random.choice(string.ascii_lowercase) for _ in range(16))
+    file_name += f'.{extension}'
+    request.urlretrieve(url, file_name)
+    await bot.send_document(cb.from_user.id, open(file_name, 'rb'))
+    os.remove(file_name)
+
+
+async def aggregate_messages(content, limit=5):
     global all_books
     books_amount = len(content)
-    ans = f'Книг знайдено: ***{books_amount}***\n'
-
-    books = await searcher.find_downloadable_formats(content[:limit])
-
+    messages_data = {}
     if limit is not None and books_amount > limit:
         all_books = content
-        ans = f'Книг знайдено: ***{books_amount}*** (перші {limit} відображені):\n'
 
-    ans += await message_helper.compile_message(books)
+    books = await searcher.find_downloadable_formats(content[:limit])
+    for book in books:
+        text, buttons = await message_helper.compile_message(book)
+        messages_data[text] = buttons
 
     all_books = [book for book in all_books if book not in books]
-
-    return ans
+    return messages_data
 
 
 if __name__ == '__main__':
--- src/message_helper.py
+++ src/message_helper.py
@@ -1,19 +1,15 @@
 import emoji
+from aiogram import types
 
 
-async def compile_message(books_list: list):
-    msg = ''
+async def compile_message(book):
+    buttons = []
+    for fmt, link in book.get('fmts').items():
+        buttons.append(types.InlineKeyboardButton(text=fmt.upper(), callback_data=f'download**{link}'))
 
-    # For now send back only first five findings
-    for book in books_list:
-        links = ''
-        for fmt, link in book.get('fmts').items():
-            links += f"{emoji.emojize(':link:')} : [{fmt}]({link})\n            "
+    msg = f"""
+        {emoji.emojize(':books:')} : {book.get('book_name', 'Книга без назви')}
+        {emoji.emojize(':memo:')} : {book.get('author', 'Автор не виявлен')}
+        """.replace('_', ' ')
 
-        msg += f"""
-            {emoji.emojize(':books:')} : {book.get('book_name', 'Книга без назви')}
-            {emoji.emojize(':memo:')} : {book.get('author', 'Автор не виявлен')}
-            {links}
-            -------------------------------------------------------
-            """
-    return msg
+    return msg, buttons
